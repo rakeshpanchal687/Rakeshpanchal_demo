1c0af37a37474268833b31818300b9f94236bcb6
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-14 13:25:16 +0530

    Program  Sum Three Num

---
+++ CWH_Ch1_PS.java
@@ -0,0 +1,10 @@
+public class CWH_Ch1_PS {
+    public static void main(String[] args) {
+        //Qustion1 :- sum three num
+        int a = 4;
+        int b = 16;
+        int c = 15;
+        int sum = a+ b+ c;
+        System.out.println(sum);
+    }
+}
