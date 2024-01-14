26cba307408ad97a877489866d4511c28afdf251
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-14 13:44:35 +0530

    Calculate  CGPA

--- CWH_Ch1_PS.java
+++ CWH_Ch1_PS.java
@@ -1,10 +1,17 @@
 public class CWH_Ch1_PS {
     public static void main(String[] args) {
         //Qustion1 :- sum three num
-        int a = 4;
-        int b = 16;
-        int c = 15;
-        int sum = a+ b+ c;
-        System.out.println(sum);
+      //  int a = 4;
+        //int b = 16;
+       // int c = 15;
+       // int sum = a+ b+ c;
+       // System.out.println(sum);
+    // Qustion2 :-     calcutate CGPA subject1 = 45;
+      float subject1 = 45;
+      float subject2 = 95;
+      float subject3 = 48;
+     float cgpa = ( subject1 + subject2 + subject3 )/30;
+        System.out.println(cgpa);
+
     }
 }
