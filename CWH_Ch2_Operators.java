dc4910fbb28b557488d28b545678bb8b3564f053
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-14 18:43:28 +0530

    Operates
    1. Arithmetic Operators
    2. Modular Operators
    3. Assignment  Operators
    4. Comparison Operators
    5. Logical Operators
    6. Bitwise Operators

---
+++ CWH_Ch2_Operators.java
@@ -0,0 +1,22 @@
+public class CWH_Ch2_Operators {
+    public static void main(String[] args) {
+        int a = 4;//Arithmetic Operaters
+        int b = 6 % a ; // Modular Operators
+        System.out.println(b);
+
+        int c = 9; //Assignment Operaters
+        c *= 3;
+        System.out.println(c);
+
+       System.out.println(64<6);// Comparison Oprators
+
+       System.out.println(64>5 && 64>8);//Logical Operators
+       System.out.println(64>5 || 64>98);//Logical Operators
+       System.out.println(2&3);// Bitwise Operators
+
+
+
+
+
+    }
+}
