e3186429c11c52262c33564826a65f9df5a1963f
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-14 14:08:04 +0530

    Program Practice set
    1. sum three num
    2. Calculate CGPA
    3. Type "hello+ name+ have a good day"
    4.Detect num. enter by the user is integer or not

--- CWH_Ch1_PS.java
+++ CWH_Ch1_PS.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CWH_Ch1_PS {
     public static void main(String[] args) {
         //Qustion1 :- sum three num
@@ -7,11 +9,22 @@ public class CWH_Ch1_PS {
        // int sum = a+ b+ c;
        // System.out.println(sum);
     // Qustion2 :-     calcutate CGPA subject1 = 45;
-      float subject1 = 45;
-      float subject2 = 95;
-      float subject3 = 48;
-     float cgpa = ( subject1 + subject2 + subject3 )/30;
-        System.out.println(cgpa);
+   //   float subject1 = 45;
+   //   float subject2 = 95;
+    //  float subject3 = 48;
+   //  float cgpa = ( subject1 + subject2 + subject3 )/30;
+    //    System.out.println(cgpa);
+
+  // Qustion 3 type hello " name " have a good day
+//        System.out.println("what is your name " );
+//        Scanner sc = new Scanner(System.in );
+//        String name = sc.next();
+//        System.out.println( "hello"  +  name + "have a good day...!");
+
+        //Qustion 4
+        System.out.println(" Enter your number " );
+        Scanner sc = new Scanner(System.in );
+        System.out.println(sc.hasNextInt());
 
     }
 }
