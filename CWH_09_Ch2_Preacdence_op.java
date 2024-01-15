0e76261b958586fb0de9fc25d311f4471e7e48cc
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-15 14:01:34 +0530

    Preacdence of operators

---
+++ CWH_09_Ch2_Preacdence_op.java
@@ -0,0 +1,21 @@
+public class CWH_09_Ch2_Preacdence_op {
+    public static void main(String[] args) {
+        int a = 6*5-34/2;
+        /*
+        =30-34/2
+        =30-17
+        =13
+         */
+        int b = 60/5-34*2;
+        /*
+        =12-34*2;
+        =12-68
+        =-56
+         */
+
+        System.out.println(a);
+        System.out.println(b);
+        //Precedence & Associativity
+        
+    }
+}
