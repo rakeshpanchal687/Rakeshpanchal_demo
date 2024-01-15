218e66a95f0c6e6756b7ffb09c10f82876917406
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-15 15:18:02 +0530

    Resulting Data type java
    Increment and Decrement Operators

---
+++ CWH_10_Resulting_data_type.java
@@ -0,0 +1,26 @@
+public class CWH_10_Resulting_data_type {
+    public static void main(String[] args) {
+        byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y + z;
+        float b = 6.54f + x;
+        System.out.println(b);
+
+      // Increment and Decrement Operators
+        int i = 56;
+      //int b = i++; // first B is assingned i (56) then i is incremented.
+        int j = 67;
+        int c = ++j; // first j is incremented then c is assingned j (68)
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println(j++);
+        System.out.println(j);
+        System.out.println(++j);
+
+
+
+    }
+}
