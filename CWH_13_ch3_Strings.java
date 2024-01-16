76f5d10723355de81618a480c542b2094be74fb7
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-16 12:46:54 +0530

    String

---
+++ CWH_13_ch3_Strings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CWH_13_ch3_Strings {
+    public static void main(String[] args) {
+        //  String name = new String("Rakesh");
+        //   String name = "Rakesh";
+        //   System.out.print("The name is: ");
+        //   System.out.println("name");
+       // int a = 6;
+       // float b = 5.6454f;
+       // System.out.printf("The value of a is %d and value of b is %f", a, b);
+       // System.out.format("The value of a is %d and value of b is %f", a, b);
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine(); // next print
+        System.out.println(st);
+
+
+     }
+    }
