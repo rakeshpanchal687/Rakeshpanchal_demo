fcc6ccf575bbe20ea0287f047218cecc9fd76804
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-18 19:06:07 +0530

    Switch Case Statements in Java

---
+++ CWH_18_elseif.java
@@ -0,0 +1,68 @@
+
+import java.util.Scanner;
+
+public class CWH_18_elseif {
+    public static void main(String[] args) {
+        String var = "Shubham";
+
+
+       switch (var) {
+            case "Shubham" -> System.out.println("You are going to become an Adult!");
+            case "Saurabh" -> System.out.println("You are going to join a Job!");
+            case "Vishaka" -> System.out.println("You are going to get retired!");
+            default -> System.out.println("Enjoy Your life!");
+        }
+        System.out.println("Thanks for using my Java Code!");
+
+
+
+
+
+
+        int age;
+        System.out.println("Enter Your Age");
+        Scanner sc = new Scanner(System.in);
+        age = sc.nextInt();
+       if (age>56){
+            System.out.println("You are experienced!");
+        }
+        else if(age>46){
+            System.out.println("You are semi-experienced!");
+        }
+        else if (age>36) {
+            System.out.println("You are semi-semi-experienced!");
+
+        }
+        else {
+            System.out.println("You are not experienced");
+        }
+        if (age>2){
+            System.out.println("You are not a baby!");
+        }
+
+
+        System.out.println("Enter Your Age");
+        sc = new Scanner(System.in);
+        age = sc.nextInt();
+        switch (age){
+            case 18:
+                System.out.println("You are going to become an Adult!");
+                break;
+            case 23:
+                System.out.println("You are going to join a job!");
+            case 60:
+                System.out.println("You are going to get retired!");
+                break;
+            default:
+                System.out.println("Enjoy Your life!");
+        }
+        System.out.println("Thanks for using my java code!");
+
+
+
+
+    }
+}
+
+
+
