5264512fc20ffb62244293aa8f2e0e266d42bcf5
rakeshpanchal687 <156292774+rakeshpanchal687@users.noreply.github.com>
2024-01-18 14:15:38 +0530

    Conditionals....
    if / else

---
+++ CWH_16_conditionals.java
@@ -0,0 +1,15 @@
+public class CWH_16_conditionals {
+    public static void main(String[] args) {
+       int age = 20;
+        //if(age>18){
+        boolean cond = (age==18);
+        if (cond){
+
+
+            System.out.println("Yes boy you can drive!");
+        }
+        else {
+            System.out.println("No boy you can not drive yet!");
+        }
+    }
+}
